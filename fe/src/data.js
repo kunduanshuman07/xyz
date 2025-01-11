a45a1cd36e6347280cf25de853401b34aaf417a5
Anshuman Kundu <kundu4coding@gmail.com>
2025-01-12 00:01:05 +0530

    landing page

---
+++ fe/src/data.js
@@ -0,0 +1,9 @@
+export const homeCards = [
+    {label:"Skill",path:"/skill"},
+    {label:"Expense",path:"/expense"},
+    {label:"Timesheet",path:"/timesheet"},
+    {label:"PMS",path:"/pms"},
+    {label:"Taskboard",path:"/taskboard"},
+    {label:"Support",path:"/support"},
+
+];
